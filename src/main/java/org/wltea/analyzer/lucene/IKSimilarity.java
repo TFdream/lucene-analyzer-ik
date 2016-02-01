875e94e2056ddc9a827f1f7f97f5aee2daea12b9
FBing <vricky.feng@gmail.com>
2016-02-01 14:01:19 +0800

    first commit

---
+++ src/main/java/org/wltea/analyzer/lucene/IKSimilarity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.wltea.analyzer.lucene;
+
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+
+/**
+ * IK Analyzer v3.2
+ * 相似度评估器
+ * 重载了DefaultSimilarity的coord方法
+ * 提高词元命中个数在相似度比较中的权重影响，即，当有多个词元得到匹配时，文档的相似度将提高
+ * @author 林良益
+ *
+ */
+public class IKSimilarity extends ClassicSimilarity {
+
+	@Override
+	public float coord(int overlap, int maxOverlap) {
+		float overlap2 = (float)Math.pow(2, overlap);
+		float maxOverlap2 = (float)Math.pow(2, maxOverlap);
+		return (overlap2 / maxOverlap2);
+	}	
+}
