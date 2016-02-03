ba07f6432b81b34e3ee387fd7e7dee5192021e04
FBing <vricky.feng@gmail.com>
2016-02-03 17:50:36 +0800

    update ext dictionary

--- src/test/java/org/wltea/analyzer/ik_analyzer5/AppTest.java
+++ src/test/java/org/wltea/analyzer/ik_analyzer5/AppTest.java
@@ -1,38 +1,58 @@
 package org.wltea.analyzer.ik_analyzer5;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.junit.Test;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+    
+	@Test
+	public void testIK(){
+		
+//		String text = "拿渡麻辣香锅味多美比肯德基好吃,日本料理比海鲜烤肉小吃";
+		String text = "艾尚瑜伽悦体中心";
+		
+		Analyzer analyzer = new IKAnalyzer(true);
+		TokenStream ts = null;
+		try {
+			ts = analyzer.tokenStream("myfield", new StringReader(text));
+			//获取词元位置属性
+		    OffsetAttribute  offset = ts.addAttribute(OffsetAttribute.class); 
+		    //获取词元文本属性
+		    CharTermAttribute term = ts.addAttribute(CharTermAttribute.class);
+		    //获取词元文本属性
+		    TypeAttribute type = ts.addAttribute(TypeAttribute.class);
+		    
+		    //重置TokenStream（重置StringReader）
+			ts.reset(); 
+			//迭代获取分词结果
+			while (ts.incrementToken()) {
+			  System.out.println(offset.startOffset() + " - " + offset.endOffset() + " : " + term.toString() + " | " + type.type());
+			}
+			//关闭TokenStream（关闭StringReader）
+			ts.end();   // Perform end-of-stream operations, e.g. set the final offset.
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			//释放TokenStream的所有资源
+			if(ts != null){
+		      try {
+				ts.close();
+		      } catch (IOException e) {
+				e.printStackTrace();
+		      }
+			}
+	    }
+	}
 }
